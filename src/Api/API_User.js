387ab6886bd554ddc5b29022bbef43ffbad75803
DESKTOP-VDMI65C\PC <contactwork.huytc@gmail.com>
2024-10-21 14:34:14 +0700

    Easeifytunes_front-end_UPDATE-3

---
+++ src/Api/API_User.js
@@ -0,0 +1,98 @@
+const API_USER = {
+  getUser: () => {
+    return [
+      {
+        id: "user01",
+        name: "User01",
+        yearOfBirth: "1993-10-17",
+        address: "153 Nguyễn Tiểu La, District 10, Ho Chi Minh City, Vietnam",
+        email: "user01@gmail.com",
+        phoneNumber: "0386479893",
+        passWord: "0123456789",
+        recentTrack: [
+          {
+            recentId: "",
+            recentLink: "",
+            recentTitle: "",
+            recentPerformer: "",
+          },
+        ],
+        yourPlayList: [],
+      },
+      {
+        id: "user02",
+        name: "User02",
+        yearOfBirth: "1993-10-28",
+        address: "88 Chu Văn An, District 6, Ho Chi Minh City, Vietnam",
+        email: "user02@gmail.com",
+        phoneNumber: "0386479893",
+        passWord: "0123456789",
+        recentTrack: [
+          {
+            recentId: "",
+            recentLink: "",
+            recentTitle: "",
+            recentPerformer: "",
+          },
+        ],
+        yourPlayList: [],
+      },
+      {
+        id: "user03",
+        name: "User03",
+        yearOfBirth: "1998-03-09",
+        address: "",
+        email: "user03@gmail.com",
+        phoneNumber: "0386479893",
+        passWord: "0123456789",
+        recentTrack: [
+          {
+            recentId: "",
+            recentLink: "",
+            recentTitle: "",
+            recentPerformer: "",
+          },
+        ],
+        yourPlayList: [],
+      },
+      {
+        id: "user04",
+        name: "User04",
+        yearOfBirth: "1992-12-23",
+        address: "",
+        email: "user04@gmail.com",
+        phoneNumber: "0386479893",
+        passWord: "0123456789",
+        recentTrack: [
+          {
+            recentId: "",
+            recentLink: "",
+            recentTitle: "",
+            recentPerformer: "",
+          },
+        ],
+        yourPlayList: [],
+      },
+      {
+        id: "user05",
+        name: "User05",
+        yearOfBirth: "2005-04-28",
+        address: "",
+        email: "user05@gmail.com",
+        phoneNumber: "0386479893",
+        passWord: "0123456789",
+        recentTrack: [
+          {
+            recentId: "",
+            recentLink: "",
+            recentTitle: "",
+            recentPerformer: "",
+          },
+        ],
+        yourPlayList: [],
+      },
+    ];
+  },
+};
+
+export default API_USER;
